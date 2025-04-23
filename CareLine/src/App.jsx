98b5724e1331426528fee37a39045bd6955e192c
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-04-23 10:09:47 -0300

    feat.servicos_basic

--- CareLine/src/App.jsx
+++ CareLine/src/App.jsx
@@ -2,12 +2,13 @@ import Header from "./components/Header"
 import Main from "./components/Landing"
 import Footer from "./components/footer"
 import Contact from "./components/Contact"
+import Servicos from "./components/Servicos"
 
 export default function App(){
   return (
     <>
       <Header />
-      <Contact />
+      <Servicos />
       <Footer />
     </>
   )
--- CareLine/src/components/Servicos.jsx
+++ CareLine/src/components/Servicos.jsx
@@ -1,5 +1,49 @@
-export default function servico(){
+import "./Servicos.css"
+
+export default function Servicos(){
     return (
-        <h1></h1>
+        <>
+            <section className="sec-1">
+                <div className="container">
+                    <div className="number-container">
+                        <p>#1</p>
+                        <img src="#" alt="icon" />
+                    </div>
+                    <h1>Painel de Gestão Operacional <br />em Tempo Real</h1>
+                </div>
+                <div className="container">
+                    <div className="number-container">
+                        <p>#2</p>
+                        <img src="#" alt="icon" />
+                    </div>
+                    <h1>Canal Digital de Comunicação <br />com Acompanhantes</h1>
+                </div>
+                <div className="container">
+                    <div className="number-container">
+                        <p>#3</p>
+                        <img src="#" alt="icon" />
+                    </div>
+                    <h1>Sistema de Feedback Imediato <br />do Paciente</h1>
+                </div>
+            </section>
+            <section className="sec-2">
+                <div className="content-button">
+                    <div className="button">
+                        <h3>ooo</h3>
+                    </div>
+                    <div className="button">
+                        <h3>aaa</h3>
+                    </div>
+                    <div className="button">
+                        <h3>uuu</h3>
+                    </div>
+                </div>
+                <div className="content-display">
+                    <h1>hello</h1>
+                </div>
+
+            </section>
+        
+        </>
     )
 }
