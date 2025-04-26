7c002eebd061e8490bc0fc947f6aa39d8bc7e0a7
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-04-26 05:27:02 -0300

    feat.responsive_footer_and_header

--- CareLine/src/components/Footer.jsx
+++ CareLine/src/components/Footer.jsx
@@ -5,21 +5,22 @@ export default function Footer(){
     return (
         <>
             <footer>
+                <div className="alberto">hello</div>
                 <div className="left-side">
                     <div className="quick-links"> 
-                    <span >
-                        <img src="./images/logo.png" alt="CareLine Logo" />
-                        <h1><a href="#">CareLine</a></h1>
-                    </span>
-                    <ul>
-                        <li><Link to="/Contato" >Contato</Link></li>
-                        <li><Link to="/" >Sobre</Link></li>
-                        <li><Link to="/Servicos" >Serviços</Link></li>
-                    </ul>
+                        <span>
+                            <img src="./images/logo.png" alt="CareLine Logo" />
+                            <h1><a href="#">CareLine</a></h1>
+                        </span>
+                        <ul>
+                            <li><Link to="/Contato">Contato</Link></li>
+                            <li><Link to="/">Sobre</Link></li>
+                            <li><Link to="/Servicos">Serviços</Link></li>
+                        </ul>
                     </div>
                     <div className="about">
                         <h1>A CareLine</h1>
-                        <p>CareLine é um software que melhora a<br></br> comunicação hospitalar, reduz o estresse <br></br>dos pacientes e otimiza o fluxo de <br></br> atendimento no pronto-socorro.</p>
+                        <p>CareLine é um software que melhora a comunicação hospitalar, reduz o estresse dos pacientes e otimiza o fluxo de atendimento no pronto-socorro.</p>
                     </div>
                 </div>
                 
@@ -28,20 +29,16 @@ export default function Footer(){
                     <form>
                         <p>Discover all our new healthcare innovations!</p>
                         <input type="email" id="email" name="email" placeholder="Enter your email here..."></input>
-                        
                     </form>
-                    <div > 
+                    <div className="social-icons"> 
                         <span className="icon">
                             <a href="#"><img src="https://www.iconpacks.net/icons/2/free-instagram-logo-icon-3497-thumb.png" alt="Instagram"/></a>
                         </span>
-                        <span className="icon" >
+                        <span className="icon">
                             <a href="https://github.com/pietroaandrade/webdev_challenge.git" target="blank"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github" /></a>
                         </span>
-                        
                     </div>
                 </div>
-            
-                
             </footer>
             <div className="copy">
                 <p>
--- CareLine/src/components/Header.jsx
+++ CareLine/src/components/Header.jsx
@@ -1,8 +1,14 @@
 import "./Header.css"
 import { Link } from "react-router"
-
+import { useState } from "react"
 
 export default function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <>
             <header>
@@ -10,14 +16,18 @@ export default function Header(){
                     <img src="./images/logo.png" alt="CareLine Logo" />
                     <h1><Link to="/" >CareLine</Link></h1>
                 </span>
-                <ul>
+                <div className="menu-icon" onClick={toggleMenu}>
+                    <div className={`bar ${isMenuOpen ? 'active' : ''}`}></div>
+                    <div className={`bar ${isMenuOpen ? 'active' : ''}`}></div>
+                    <div className={`bar ${isMenuOpen ? 'active' : ''}`}></div>
+                </div>
+                <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
                     <li><Link to="/" >Home</Link></li>
                     <li><Link to="/" >Sobre</Link></li>
                     <li><Link to="/Servicos" >Serviços</Link></li>
                     <li><Link to="/Contato" >Contato</Link></li>
                 </ul>
             </header>   
-                    
         </>
     )
 }
