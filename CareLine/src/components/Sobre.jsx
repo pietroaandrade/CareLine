6c5f9abbd2a5ffed4b485d2283fa8329b5337495
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-04-30 17:50:54 -0300

    feat.finished_sobre

--- CareLine/src/components/Sobre.jsx
+++ CareLine/src/components/Sobre.jsx
@@ -68,6 +68,85 @@ export default function Sobre() {
                     <p>{contentData[activeIndex].text}</p>
                 </div>
             </div>
+            <div className="sobre-3">
+                <img src="./images/nissan.webp" alt="Doctor caring for kid" />
+                <h3>
+                    Somos uma camada de <span>inteligência aplicada a operação hospitalar diária</span>,<br /> projetada para reduzir atritos, aumentar previsibilidade e <br />fortalecer a governança clínica
+                </h3>  
+            </div>
+            <section className="sec-6">
+                    <div className="container">
+                        <h1>Transforme o seu negócio <br /> da área da saúde por <br /> meio da tecnologia. <br /> <span>Junte-se à CareLine!</span></h1>
+                        <p>
+                            Quando você leva a CareLine para dentro da sua <br /> empresa, recebe mais do que soluções de <br /> ponta;   
+                            <span>
+                                recebe a parceria estratégica de uma <br /> companhia com expertise em tecnologia para <br /> a área da saúde.
+                            </span>
+                            <br />
+                            <br /> Preencha o formulário e solicite agora mesmo <br /> um contato comercial. Temos a solução certa <br /> esperando por você!   
+                        </p>
+                        <img src="./images/docpent.png" alt="Doutor homem sorrindo frame pentagono" />
+                    </div>
+
+                    <div>
+                        <div className="form-container">
+                            <form>
+                                <div>
+                                    <label htmlFor="name">Nome*</label>
+                                    <input type="text" id="name" name="name" required />
+                                </div>
+
+                                <div>
+                                    <label htmlFor="email">Email*</label>
+                                    <input type="email" id="email" name="email" required />
+                                </div>
+                                
+                                <div>
+                                    <label htmlFor="phone">Telefone*</label>
+                                    <div className="phone-container">
+                                        <select id="country-code" name="country-code">
+                                            <option value="55">Brasil (+55)</option>
+                                        </select>
+                                        <input type="tel" id="phone" name="phone" required />
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <label htmlFor="company">Empresa*</label>
+                                    <input type="text" id="company" name="company" required />
+                                </div>
+
+                                <div className="QA">
+                                    <label htmlFor="profile">Qual seu perfil?*</label>
+                                    <select id="profile" name="profile" required>
+                                        <option value="">Selecione</option>
+                                        <option value="perfil1">Perfil 1</option>
+                                        <option value="perfil2">Perfil 2</option>
+                                    </select>
+                                
+                                    <label htmlFor="solution">Qual solução CareLinee te interessa?*</label>
+                                    <select id="solution" name="solution" required>
+                                        <option value="">Selecione</option>
+                                        <option value="solucao1">Solução 1</option>
+                                        <option value="solucao2">Solução 2</option>
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <label htmlFor="comments">Comente um pouco mais sobre como podemos te ajudar.</label>
+                                    <textarea id="comments" name="comments" rows="2"></textarea>
+                                </div>
+
+                                <label>
+                                    <input type="checkbox" name="privacy" required />
+                                    Estou de acordo com a Política de Privacidade!
+                                </label>
+                                <a href="#" type="submit"><button><span>Solicitar Demonstração</span></button></a>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </section>
             <Footer />
         </>
     );
