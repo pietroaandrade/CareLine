fdcb00cb8b14cf2d2955e99763bf0950dee8e5a7
pietroaa <167457324+pietroaandrade@users.noreply.github.com>
2025-05-26 12:37:36 -0300

    feat.api_requirements

--- CareLine/src/components/LoginSignup/Login.jsx
+++ CareLine/src/components/LoginSignup/Login.jsx
@@ -1,35 +1,58 @@
 import React, {useState} from "react";
 
 import "./Login.css"
+
+import api from "../axios";
+
 export default function Login(){
 
     const [action,setAction] = useState("Login");
     const [state, transform] = useState("Off");
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    
     return(
         <>
-            <div className="background"><img src=".././images/login-backgroundd.jpg" alt="" /></div>
+            <div className="background"><img src="/images/login-backgroundd.jpg" alt="" /></div>
             <div className="container">
                 <div className="header">
-                    <img className="header-logo" src=".././images/logo.png" alt="" />
+                    <img className="header-logo" src="/images/logo.png" alt="" />
                     <div className="header-text">Portal CareLine</div>
                     <div className="underline"></div>
                 </div>
                 <div className="inputs">
-                    {action==="Login"? <div></div>:
-                        <div className="input">
-                            <img src=".././images/user-img.svg" alt="" />
-                            <input type="text" placeholder="Usuário" />
-                        </div>
-                    }
-                    
+                {action === "Login" ? null : (
                     <div className="input">
-                        <img src=".././images/mail-icon.svg" alt="" />
-                        <input type="text" placeholder="Email" />
+                        <img src="/images/user-img.svg" alt="" />
+                        <input
+                        type="text"
+                        placeholder="Usuário"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    )}
+
+                    <div className="input">
+                    <img src="/images/mail-icon.svg" alt="" />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                     </div>
                     <div className="input">
-                        <img src=".././images/lock-icon.svg" alt="" />
-                        <input type="password" placeholder="Senha"/>
+                    <img src="/images/lock-icon.svg" alt="" />
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                     </div>
                 </div>
                 {action==="Sign Up"? <div></div>:
@@ -42,6 +65,8 @@ export default function Login(){
                     <div className={action==="Sign Up"?"submit" : "submit gray"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
                     <div className={action==="Login"?"submit" : "submit gray"} onClick={()=>{setAction("Login")}}>Login</div>
                 </div>
+                
+
 
             </div>
         </>
